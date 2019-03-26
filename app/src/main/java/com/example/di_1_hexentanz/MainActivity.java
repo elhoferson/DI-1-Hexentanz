b6c84318cac02a57dc5a989a483b66450584c6ca
Matthias Hülser <hallo@matthias.live>
2019-03-26 22:54:22 +0100

    Created Startscreen (background, title and buttons); disabled action bar; hide status bar.

--- app/src/main/java/com/example/di_1_hexentanz/MainActivity.java
+++ app/src/main/java/com/example/di_1_hexentanz/MainActivity.java
@@ -1,7 +1,9 @@
 package com.example.di_1_hexentanz;
 
+import android.app.ActionBar;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -9,5 +11,9 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        View decorView = getWindow().getDecorView();
+        // Hide the status bar.
+        int uiOptions = View.SYSTEM_UI_FLAG_FULLSCREEN;
+        decorView.setSystemUiVisibility(uiOptions);
     }
 }
