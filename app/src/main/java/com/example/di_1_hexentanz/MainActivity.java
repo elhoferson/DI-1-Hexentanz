78766ccfa82fc3935d0e7d912fd776534b83ae7c
Matthias Hülser <hallo@matthias.live>
2019-03-27 11:39:29 +0100

    added functionality to join game button and create game button

--- app/src/main/java/com/example/di_1_hexentanz/MainActivity.java
+++ app/src/main/java/com/example/di_1_hexentanz/MainActivity.java
@@ -4,6 +4,8 @@ import android.app.ActionBar;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.View;
+import android.widget.ImageView;
+import android.widget.Toast;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -15,5 +17,21 @@ public class MainActivity extends AppCompatActivity {
         // Hide the status bar.
         int uiOptions = View.SYSTEM_UI_FLAG_FULLSCREEN;
         decorView.setSystemUiVisibility(uiOptions);
+
+        ImageView BtnJoinGame = findViewById(R.id.Btn_JoinGame);
+        BtnJoinGame.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Toast.makeText(getApplicationContext(), "Join Game Button", Toast.LENGTH_LONG).show();
+            }
+        });
+
+        ImageView BtnCreateGame = findViewById(R.id.Btn_CreateGame);
+        BtnCreateGame.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Toast.makeText(getApplicationContext(), "Create Game Button", Toast.LENGTH_LONG).show();
+            }
+        });
     }
 }
