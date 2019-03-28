71d5e98d886fa90e2550019b2cbace1cf55ea7de
Szolderits Chiara <c.szolderits@gmail.com>
2019-03-28 07:59:16 +0100

    created DiceActivity for rolling the dice

--- app/src/main/java/com/example/di_1_hexentanz/MainActivity.java
+++ app/src/main/java/com/example/di_1_hexentanz/MainActivity.java
@@ -1,114 +1,33 @@
 package com.example.di_1_hexentanz;
 
-
-import android.hardware.Sensor;
-import android.hardware.SensorEvent;
-import android.hardware.SensorEventListener;
-import android.hardware.SensorManager;
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
-import android.widget.ImageView;
+import android.view.View;
+import android.widget.Button;
 
-import java.util.Random;
 
 public class MainActivity extends AppCompatActivity {
 
-    ImageView dice;
-    SensorManager shakingSensor;
-    Sensor shakingAccelerometer;
-
-
-    private Random randomGenerator = new Random();
-    private static int SHAKE_THRESHOLD = 3;
-
+    Button btn_dice;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
-
-        dice = findViewById(R.id.dice);
-        shakingSensor = (SensorManager) getSystemService(SENSOR_SERVICE);
-        shakingAccelerometer = shakingSensor.getDefaultSensor(Sensor.TYPE_ACCELEROMETER);
-
-
-        /*
-        dice.setOnClickListener(new View.OnClickListener() {
+        btn_dice = findViewById(R.id.btn_dice);
+        btn_dice.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
-                rollDice();
+                Intent i = new Intent(getApplicationContext(), DiceActivity.class);
+                startActivity(i);
             }
         });
 
-        */
 
 
     }
 
 
-    private final SensorEventListener shakingListener = new SensorEventListener() {
-        @Override
-        public void onSensorChanged(SensorEvent event) {
-            float x = event.values[0];
-            float y = event.values[1];
-            float z = event.values[2];
-
-            float acceleration = (float) Math.sqrt(x * x + y * y + z * z) - SensorManager.GRAVITY_EARTH;
-
-
-            if (acceleration > SHAKE_THRESHOLD)
-                rollDice();
-
-        }
-
-        @Override
-        public void onAccuracyChanged(Sensor sensor, int accuracy) {
-
-        }
-    };
-
-
-    @Override
-    protected void onResume() {
-        super.onResume();
-        shakingSensor.registerListener(shakingListener,
-                shakingSensor.getDefaultSensor(Sensor.TYPE_ACCELEROMETER),
-                SensorManager.SENSOR_DELAY_NORMAL);
-    }
-
-    @Override
-    protected void onPause() {
-        shakingSensor.unregisterListener(shakingListener);
-        super.onPause();
-    }
-
-
-    public void rollDice() {
-
-        int randomNumber = randomGenerator.nextInt(6) + 1;
-
-        switch (randomNumber) {
-            case 1:
-                dice.setImageResource(R.drawable.dice1);
-                break;
-            case 2:
-                dice.setImageResource(R.drawable.dice2);
-                break;
-            case 3:
-                dice.setImageResource(R.drawable.dice3);
-                break;
-            case 4:
-                dice.setImageResource(R.drawable.dice4);
-                break;
-            case 5:
-                dice.setImageResource(R.drawable.dice5);
-                break;
-            case 6:
-                dice.setImageResource(R.drawable.dice6);
-                break;
-        }
-
-
-    }
 }
