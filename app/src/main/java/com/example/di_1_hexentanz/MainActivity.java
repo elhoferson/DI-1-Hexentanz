c2df8908f4b33d51afb0db3e7bc1c80941a6c954
Daniel Hofmann <Dahofmann@edu.aau.at>
2019-04-01 12:46:47 +0200

    set background image to "spielbrett.jpg", hide the TitleBar

--- app/src/main/java/com/example/di_1_hexentanz/MainActivity.java
+++ app/src/main/java/com/example/di_1_hexentanz/MainActivity.java
@@ -2,12 +2,20 @@ package com.example.di_1_hexentanz;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.Window;
+import android.view.WindowManager;
 
 public class MainActivity extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
+
+        try {
+            this.getSupportActionBar().hide();
+        }catch (NullPointerException npe){
+            npe.printStackTrace();
+        }
         setContentView(R.layout.activity_main);
     }
 }
