245bb5b9946e03ac45dd0e7cbc2f1a94c1cf3974
Szolderits Chiara <c.szolderits@gmail.com>
2019-04-01 07:46:28 +0200

    added WalkingActivity and flexible status bar

--- app/src/main/java/com/example/di_1_hexentanz/WalkingActivity.java
+++ app/src/main/java/com/example/di_1_hexentanz/WalkingActivity.java
@@ -1,15 +1,27 @@
 package com.example.di_1_hexentanz;
 
+import android.app.Activity;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.view.Window;
+import android.view.WindowManager;
 
-public class WalkingActivity extends AppCompatActivity {
+public class WalkingActivity extends Activity {
 
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_walking);
+
+        View decorView = getWindow().getDecorView();
+        // Hide the status bar.
+        // int uiOptions = View.SYSTEM_UI_FLAG_FULLSCREEN;
+        decorView.setSystemUiVisibility(View.SYSTEM_UI_FLAG_HIDE_NAVIGATION
+                | View.SYSTEM_UI_FLAG_IMMERSIVE_STICKY
+                | View.SYSTEM_UI_FLAG_FULLSCREEN);
+
     }
 
     public static int walkFields(int numberOfFields) {
