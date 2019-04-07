7693570ce83e96b58c41b7dcb01289d0fb0685eb
Matthias Hülser <hallo@matthias.live>
2019-04-07 20:16:30 +0200

    renamed activity to startscreen to avoid conflicts when merging

--- app/src/main/java/com/example/di_1_hexentanz/Sartscreen.java
+++ app/src/main/java/com/example/di_1_hexentanz/Sartscreen.java
@@ -1,18 +1,17 @@
 package com.example.di_1_hexentanz;
 
-import android.app.ActionBar;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.ImageView;
 import android.widget.Toast;
 
-public class MainActivity extends AppCompatActivity {
+public class Sartscreen extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_main);
+        setContentView(R.layout.activity_startscreen);
         View decorView = getWindow().getDecorView();
         decorView.setSystemUiVisibility(View.SYSTEM_UI_FLAG_HIDE_NAVIGATION
                 | View.SYSTEM_UI_FLAG_IMMERSIVE_STICKY
