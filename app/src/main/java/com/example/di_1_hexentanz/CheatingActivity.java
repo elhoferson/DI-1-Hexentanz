05bf7cc1f7393d339fa8b35de7f64f825b29edf2
Daniel Hofmann <Dahofmann@edu.aau.at>
2019-04-08 19:05:07 +0200

    implemented SensorEventManager, setup SensorManager, setup a Sensor of type light

--- app/src/main/java/com/example/di_1_hexentanz/CheatingActivity.java
+++ app/src/main/java/com/example/di_1_hexentanz/CheatingActivity.java
@@ -1,9 +1,59 @@
 package com.example.di_1_hexentanz;
 
 import android.app.Activity;
+import android.app.AlertDialog;
+import android.app.Service;
+import android.content.DialogInterface;
+import android.hardware.Sensor;
+import android.hardware.SensorEvent;
+import android.hardware.SensorEventListener;
+import android.hardware.SensorManager;
+import android.os.Bundle;
+import android.widget.ImageView;
+import android.widget.TextView;
 
-public class CheatingActivity extends Activity {
+public class CheatingActivity extends Activity implements SensorEventListener {
 
+    TextView textView;
+    SensorManager sensorManager;
+    Sensor sensor;
+    ImageView background;
 
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        //TODO: Connect textView
+
+        background = findViewById(R.id.imageView);
+        textView.bringToFront();
+        background.invalidate();
+        sensorManager = (SensorManager) getSystemService(Service.SENSOR_SERVICE);
+        sensor = sensorManager.getDefaultSensor(Sensor.TYPE_LIGHT);
+
+    }
+
+    @Override
+    protected void onPause() {
+        super.onPause();
+        sensorManager.unregisterListener(this);
+    }
+
+    @Override
+    protected void onResume() {
+        super.onResume();
+        sensorManager.registerListener(this, sensor, SensorManager.SENSOR_DELAY_NORMAL);
+    }
+
+    @Override
+    public void onSensorChanged(SensorEvent event) {
+
+    }
+
+    @Override
+    public void onAccuracyChanged(Sensor sensor, int accuracy) {
+
+    }
 
 }
