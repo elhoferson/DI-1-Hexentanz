d6813702a2e732b437f1d53309bfc23486c6f646
Matthias Hülser <hallo@matthias.live>
2019-04-08 17:02:23 +0200

    added gamescreen activity

---
+++ app/src/main/java/com/example/di_1_hexentanz/Gamescreen.java
@@ -0,0 +1,13 @@
+package com.example.di_1_hexentanz;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class Gamescreen extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_gamescreen);
+    }
+}
