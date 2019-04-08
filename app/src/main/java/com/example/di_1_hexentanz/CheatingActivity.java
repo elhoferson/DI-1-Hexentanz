dfb0d4b2a8412c756ee97ae3630305d6830c0d05
Daniel Hofmann <Dahofmann@edu.aau.at>
2019-04-08 19:09:12 +0200

    implemented onSensorChanged:  - read Luminosity  - write Luminosity to textView (debugging)  - AlertDialog.Builder  - show AlertDialog

--- app/src/main/java/com/example/di_1_hexentanz/CheatingActivity.java
+++ app/src/main/java/com/example/di_1_hexentanz/CheatingActivity.java
@@ -48,7 +48,39 @@ public class CheatingActivity extends Activity implements SensorEventListener {
 
     @Override
     public void onSensorChanged(SensorEvent event) {
+        if (event.sensor.getType() == Sensor.TYPE_LIGHT) {
+            //Light Sensor action
+            textView.setText("Luminosity: " + event.values[0]);
 
+            if (event.values[0] < 5) {
+
+
+                sensorManager.unregisterListener(this);
+
+                AlertDialog.Builder a_builder = new AlertDialog.Builder(CheatingActivity.this);
+                a_builder.setMessage("It is dark and cloudy tonight. The New Moon is rising in the sky, but it is barely giving off light. " +
+                        "This may be an opportunity for you! You look around but you don't see anybody. Do you want to cheat?")
+                        .setCancelable(false)
+                        .setPositiveButton("Yes!", new DialogInterface.OnClickListener() {
+                            @Override
+                            public void onClick(DialogInterface dialog, int which) {
+                                //Yes I want to cheat
+                                textView.setText("CHEAT? REALLY?!? COME ON!!");
+                            }
+                        })
+                        .setNegativeButton("No", new DialogInterface.OnClickListener() {
+                            @Override
+                            public void onClick(DialogInterface dialog, int which) {
+                                //No I dont want to cheat
+                                textView.setText("Your are a fair player.. but what about the others?");
+                            }
+                        });
+
+                AlertDialog alert = a_builder.create();
+                alert.show();
+
+            }
+        }
     }
 
     @Override
