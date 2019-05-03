880390552c5e98d1e5484dcef8bc2915ea93d84f
Szolderits Chiara <c.szolderits@gmail.com>
2019-05-03 14:42:15 +0200

    reformatted code

--- app/src/test/java/com/example/di_1_hexentanz/DiceTest.java
+++ app/src/test/java/com/example/di_1_hexentanz/DiceTest.java
@@ -5,18 +5,12 @@ import org.junit.Test;
 
 import java.util.Arrays;
 import java.util.List;
-import org.junit.Test;
-
-import static org.hamcrest.CoreMatchers.containsString;
-import static org.hamcrest.CoreMatchers.is;
-import static org.junit.Assert.*;
-
 
 
 public class DiceTest {
 
     Dice testDice;
-    List<Integer> range = Arrays.asList(1,2,3,4,5,6);
+    List<Integer> range = Arrays.asList(1, 2, 3, 4, 5, 6);
 
 
     @Before
@@ -25,21 +19,16 @@ public class DiceTest {
     }
 
 
-
     @Test
     public void testCorrectRandomNumberAlgorithm() {
-        for(int i = 0; i<=1000000; i++) {
+        for (int i = 0; i <= 1000000; i++) {
             if (range.contains(testDice.getRandomNumber())) {
-                //System.out.println("OK");
                 break;
-
-
-        }
+            }
             throw new IllegalArgumentException("Dice number range is only from 1 to 6");
         }
-
-
-
-        }
     }
+
+
+}
 
